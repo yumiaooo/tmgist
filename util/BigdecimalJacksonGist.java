be2ecaecacce9f1625562d79b559b5d860120e5d
Miles Yu <yumiaooo@gmail.com>
2015-12-14 15:46:29 +0800

    BigDecimal as String

--- util/BigdecimalJacksonGist.java
+++ util/BigdecimalJacksonGist.java
@@ -2,6 +2,8 @@ package com.creditease.tradematch.tmfront.gist.util;
 
 import com.creditease.tradematch.tmfront.dto.InvestReq;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 import java.io.IOException;
 import java.math.BigDecimal;
@@ -11,7 +13,8 @@ import java.math.BigDecimal;
  */
 public class BigdecimalJacksonGist {
     public static void main(String[] argv) throws IOException {
-        String json = "{\"value\":3.141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279}";
+        String json = "{\"value\":\"3.141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279141592653589793238462643383279\"}";
+//        String json = "{\"value\":\"10000.00\"}";
 //        String json = "{\"value\":\"3.141592653589793238462643383279\"}";
 
         ObjectMapper mapper = new ObjectMapper();
@@ -26,7 +29,9 @@ public class BigdecimalJacksonGist {
     }
 
     public static class SomeDTO {
+//        @JsonSerialize(using = ToStringSerializer.class)
         private BigDecimal value;
+
         private Double d;
 
         public BigDecimal getValue() {
--- util/JacksonGist.java
+++ util/JacksonGist.java
@@ -1,6 +1,7 @@
 package com.creditease.tradematch.tmfront.gist.util;
 
 import com.creditease.tradematch.domain.TransferFormRequest;
+import com.creditease.tradematch.tmfront.dto.InvestReq;
 import com.creditease.tradematch.tmfront.dto.TransferReq;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.common.base.Charsets;
@@ -14,25 +15,38 @@ import java.net.URL;
  *
  */
 public class JacksonGist {
+    static ObjectMapper mapper = new ObjectMapper();
+
     public static void main2(String[] argv) throws IOException {
         URL url = Resources.getResource("json/putTransferReq.json");
         String json = Resources.toString(url, Charsets.UTF_8);
 
-        ObjectMapper mapper = new ObjectMapper();
         TransferReq req = mapper.readValue(json, TransferReq.class);
 
         String json2 = mapper.writeValueAsString(req);
         System.out.println(json2);
     }
 
-    public static void main(String[] argv) throws IOException {
+    public static void main3(String[] argv) throws IOException {
 //        TransferReq req = new TransferReq();
 //        req.setFinanceAmount(new BigDecimal("10000.00"));
         TransferFormRequest apamaReq = new TransferFormRequest();
         apamaReq.setFinanceAmount(new BigDecimal("10000.00"));
 
-        ObjectMapper mapper = new ObjectMapper();
         String json2 = mapper.writeValueAsString(apamaReq);
+        System.out.println(json2);
+    }
+
+    public static void main(String[] argv) throws IOException {
+        URL url = Resources.getResource("json/putInvestReq.json");
+        String json = Resources.toString(url, Charsets.UTF_8);
+
+        InvestReq req = mapper.readValue(json, InvestReq.class);
+
+        System.out.println(req);
+        System.out.println();
+
+        String json2 = mapper.writeValueAsString(req);
         System.out.println(json2);
     }
 }
